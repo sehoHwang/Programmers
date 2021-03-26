f6051d074a7d9bf94e876bd00a36eaaf8d5db09a
seho Hwang <seho4815@gmail.com>
2021-03-26 18:41:31 +0900

    수리공항승-1449.cpp

---
+++ Baekjoon/그리디/수리공항승.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int N, L, l;
+    int answer = 0;
+    vector<int> loc;
+    scanf("%d %d", &N, &L);
+    for(int i=0; i<N; i++){
+        scanf("%d", &l);
+        loc.push_back(l);
+    }
+
+    sort(loc.begin(), loc.end());
+    int idx = 0;
+    while(1){
+        if(idx == loc.size()-1){
+            answer++;
+            break;
+        }
+        int tmp = loc[idx];
+        int jump = 0;
+        while(loc[idx+jump+1]-loc[idx] <= L-1){
+            jump++;
+        }
+        idx += jump;
+        answer+=1;
+        
+        idx++;
+    }
+    
+    cout<<answer;
+
+    return 0;
+}
