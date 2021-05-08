3dafe512a5977ffb36046e39cc5a66d716b62fa2
Hwang Seho <seho4815@gmail.com>
2021-05-08 23:31:20 +0900

    2020 카카오 인턴십 동물 탐험 dfs 방식 new sol

--- kakao 기출/2020인턴십-동굴 탐험.cpp
+++ kakao 기출/2020인턴십-동굴 탐험.cpp
@@ -55,3 +55,45 @@ bool solution(int n, vector<vector<int>> path, vector<vector<int>> order) {
     for(int i = 0 ; i < n ; i++) if(!check_visit[i]) return false;
     return true;
 }
+
+/*
+    dfs 풀이
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN=2e5;
+bool vis[MAXN];
+int prior[MAXN],hang[MAXN];
+vector<int> edge[MAXN];
+
+void visit(int v){
+    if(vis[v])return;
+    
+    if(!vis[prior[v]]){		// 2
+        hang[prior[v]]=v;
+        return;
+    }
+ 
+    vis[v]=true;
+    visit(hang[v]);		// 3
+    for(int u:edge[v])visit(u);
+}
+
+bool solution(int n, vector<vector<int>> path, vector<vector<int>> order) {
+    // 1
+    for(auto &a:path){
+        edge[a[0]].push_back(a[1]);
+        edge[a[1]].push_back(a[0]);
+    }
+    for(auto &a:order)prior[a[1]]=a[0];
+
+    if(prior[0])return false;
+
+    vis[0]=true;
+    for(int u:edge[0])visit(u);
+    
+    for(int i=0;i<n;++i)if(!vis[i])return false;
+    return true;
+}
