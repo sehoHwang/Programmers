41ccce260b7954b8e45dc883c36d2e3cf9923813
Hwang Seho <seho4815@gmail.com>
2021-05-13 15:24:50 +0900

    튜플 sol

---
+++ Python/Kakao 인턴십/튜플.py
@@ -0,0 +1,38 @@
+"""
+    Solution
+
+    1. '{' 와 '}'를 먼저 제거한다.
+
+    2. ','를 기준으로 list에 저장한다.
+
+    3. dictionary를 사용해 각 숫자를 key로 등장 횟수를 value로 저장한다.
+
+    4. dict_tuple 형태로 (dict.items()) 변환해 내림차순 정렬을 하고 result에 담는다.
+
+    5. result를 순회하며 answer.append()
+"""
+
+def solution(s):
+    answer = []
+    set = {}
+    s = s.replace('{','').replace('}', '')
+    idx = 0
+    for i in range(len(s)):
+        if s[i]==',':
+            tmp = s[idx:i]
+            if tmp in set:
+                set[tmp]+=1
+            else:
+                set[tmp]=1
+            idx = i+1
+    tmp = s[idx:len(s)]
+    if tmp in set:
+        set[tmp]+=1
+    else:
+        set[tmp] = 1
+    
+    result = sorted(set.items(), key = lambda x:x[1], reverse= True)
+    for x in result:
+        answer.append(int(x[0]))
+    
+    return answer
