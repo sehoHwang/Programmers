89b579aa0784541725a98d33d58a068561e605d7
Hwang Seho <seho4815@gmail.com>
2021-06-16 16:30:46 +0900

    bfs, 주석 수정

--- Python/BoJ/2583.py
+++ Python/BoJ/2583.py
@@ -1,7 +1,6 @@
 from collections import deque
 
 def tiling(x1, y1, x2, y2):
-    #print(x1, y1, x2,y2)
     for i in range(y2, y1+1):
         for j in range(x1, x2+1):
             board[i][j] = 1
@@ -12,15 +11,12 @@ def bfs(y, x):
     q.append([y,x])
     while q:
         cur_y, cur_x = q.popleft()
-        #print("현재 좌표  :", cur_y, cur_x)
         cnt+=1
-        #print(cur_y, cur_x, cnt)
         for i in range(4):
             new_y, new_x = cur_y+dy[i], cur_x+dx[i]
             if 0<=new_y<M and 0<=new_x<N and board[new_y][new_x]==0:
                 q.append([new_y, new_x])
                 board[new_y][new_x] = -1
-                #print("삽입 좌표, 상태 : ", new_y, new_x, board[new_y][new_x])
     return cnt
 
 
@@ -39,7 +35,6 @@ for _ in range(K):
     y2 = M-1-y2
 
     tiling(x1, y1, x2, y2)
-#print(board)
 
 for i in range(M):
     for j in range(N):
@@ -47,7 +42,6 @@ for i in range(M):
             board[i][j] = -1
             count.append(bfs(i,j))
             req_cnt +=1
-            #print(">>> " , board)
 count.sort()
 print(req_cnt)
 for c in count:
